548f792b1c9f7363bada6239af46b0b1fd1b280a
GenericallyTerrible <jmoehrnkel@gmail.com>
2016-02-21 15:35:08 -0500

    Added breakpoint class and test for it

---
+++ FingerJointAlgorithmTesting/BreakpointTesting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FingerJointAlgorithmTesting
+{
+    [TestClass]
+    public class BreakpointTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
